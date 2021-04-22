ebc9b2edebf60e6bd178dcb590ea6f87844cd8fb
N0il <sergio.r.gama@gmail.com>
2021-04-22 08:42:25 +0100

    Input Verification

--- server.c
+++ server.c
@@ -5,6 +5,14 @@
 #include <string.h>
 #include <ctype.h>
 
+/*--------------------GLOBAL VARIABLES--------------------*/
+
+char *public_fifo;  //holds the public fifo name/path
+
+bool time_is_up = false;  //to keep track of server execution time
+
+/*------------------END GLOBAL VARIABLES------------------*/
+
 
 /**
  * @brief treats user argument input errors
@@ -42,11 +50,43 @@ bool is_number(char *str){
 }
 
 int main(int argc, char* argv[]){
+    /*
+    # ARGV 0 -> ./s         ARGV 3 -> fifoname / -l   #
+    # ARGV 1 -> -t          ARGV 4 -> bufsize         #
+    # ARGV 2 -> nsecs       ARGV 5 -> fifoname        #
+    */
+
+   /*--------------------INPUT ERROR VERIFICATION--------------------*/
 
     //when less than the minimun arguments number is given
     if(argc < 4) input_error();
 
+    //when -t or time is not passed correctly
     if(!str_cmp(argv[1], "-t") || !is_number(argv[2])) input_error();
+
+    //-l passed but with no arguments
+    if(str_cmp(argv[3], "-l") && argc != 6) input_error();
+
+    //-l not passed or bufsize argmuent is not a number
+    if(argc == 6 && (!str_cmp(argv[3], "-l") || !is_number(argv[4]))) input_error();
+
+    //after this the program assumes inputs are correctly inputted
+
+    /*------------------END INPUT ERROR VERIFICATION------------------*/   
+
+    int nsecs = atoi(argv[2]);
+    int bufsize = 1;
+
+    if(str_cmp(argv[3], "-l")){
+        bufsize = atoi(argv[4]);
+        public_fifo = argv[5];
+    }
+    else{
+        public_fifo = argv[3];
+    }
+
+    while(!time_is_up){
+    }
 
     return 0;
 }
