949aaf4f1dcdd2cdb9915e4275f0dc4a56edc1d6
N0il <sergio.r.gama@gmail.com>
2021-04-22 16:03:36 +0100

    main loop and strcuture of server (work in progress)

--- server.c
+++ server.c
@@ -19,6 +19,7 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <errno.h>
 
 #define ERROR -1
 #define RECVD "RECVD"
@@ -41,6 +42,8 @@ bool time_is_up = false;  //to keep track of server execution time
 bool public_fifo_closed = true;  //to keep track of public fifo
 
 bool debug = true;  //used to keep track of debugging printfs
+
+int bufsz = 1;  //server buffer size (default 1)
 
 /*------------------END GLOBAL VARIABLES------------------*/
 
@@ -83,9 +86,19 @@ bool is_number(char *str){
 }
 
 
-void *create_producers(void * arg){
+void *producer_thread(void * arg){
+    message_t *request = (message_t*) arg;
+    //get current time
+    time_t cur_secs;
+    time(&cur_secs);
 
+    pthread_t tid = pthread_self();
+    int pid = getpid();
+   
+    fprintf(stdout, "%ld; %d; %d; %d; %lu; %d; %s\n", cur_secs, request->rid, request->tskload, pid, tid, request->tskres, RECVD);
 }
+
+void *consumer_thread(void * arg){}
 
 
 /*-------------------END UTIL FUNCTIONS-------------------*/
@@ -123,37 +136,33 @@ int main(int argc, char* argv[]){
 
     //after this the program assumes inputs are correctly inputted
 
-    /*------------------END INPUT ERROR VERIFICATION------------------*/   
+    /*------------------END INPUT ERROR VERIFICATION------------------*/ 
+
+    /*---------------------PROGRAM INITIALIZATION---------------------*/   
 
     //retrieving arguments inputted
     int nsecs = atoi(argv[2]);
-    int bufsize = 1;
-
-    //getting the current directory
-    if (getcwd(public_fifo, sizeof(public_fifo)) == NULL) {
-       fprintf(stderr, "Error getting current working dir\n");
-       return ERROR;
-    }
-
-    strcat(public_fifo, "/");
 
     if(str_cmp(argv[3], "-l")){
-        bufsize = atoi(argv[4]);
+        bufsz = atoi(argv[4]);
         strcat(public_fifo, argv[5]);
     }
     else{
         strcat(public_fifo, argv[3]);
     }
 
+    if(debug) printf("Fifo path: %s\n", public_fifo);  //DEBUG
 
-    if(debug) printf("Fifo path: %s\n", public_fifo);
+    //printing final and initial time to user and the rest of the parameters to user
+    long int nsecs_long = (long) (int) nsecs;
+    fprintf(stderr,"[server] initial time: %lu, expected final time: %lu\n", initial_time, initial_time + nsecs_long);
+    fprintf(stderr,"[server] got: nsecs=%d, bufsize=%d, fifoname=%s\n", nsecs, bufsz, public_fifo);
 
     //creating public fifo file
     if(mkfifo(public_fifo, 0777) == ERROR){
-        fprintf(stderr, "Error when creating public fifo\n");
+        fprintf(stderr, "Error when creating public fifo: %s\n", strerror(errno));
         return ERROR;                                                
     }
-    
 
     //thread unique identifier
     long int id = 1;
@@ -166,15 +175,18 @@ int main(int argc, char* argv[]){
     int tout = 1;  //DEBUG
 
     //getting the request from client
-    message_t request;
+    message_t *request;
+
+    
+    /*---------------------MAIN THREAD (C0) LOOP---------------------*/
 
     while(!time_is_up){
         //openning public fifo
-        if(public_fifo_closed) public_fd = open(public_fifo, O_RDONLY | O_NONBLOCK);
+        public_fd = open(public_fifo, O_RDONLY | O_NONBLOCK);  //NOT WORKING
         
         //when the read side it is not open
         if(public_fd == ERROR){ 
-            fprintf(stderr, "[server] Error while opening public fifo file!\n");
+            if(debug) printf("Error while opening public fifo file!\n");  //DEBUG
             public_fifo_closed = true;  
         }
         else{
@@ -183,21 +195,21 @@ int main(int argc, char* argv[]){
 
         if(!public_fifo_closed){
             //reading requests from client
-            bytes_read = read(public_fd, &request, sizeof(message_t)); 
+            bytes_read = read(public_fd, request, sizeof(message_t)); 
 
             //to check if there was a resquest or not
-            if(bytes_read != ERROR){
+            if(bytes_read > 0){
                 //creating producer threads
                 pthread_t thread_id;
 
-                if(debug) printf("created thread number: %ld \n", id); //DEBUG
+                if(debug) printf("created producer thread number: %ld\n", id); //DEBUG
 
-                if(pthread_create(&thread_id, NULL, &create_producers, (void*)id) != 0) return ERROR;
+                if(pthread_create(&thread_id, NULL, &producer_thread, (void*)request) != 0) return ERROR;
 
                 id++;
             }
             else{
-                if(debug) printf("timeout read: %d", tout);  //DEBUG
+                if(debug) printf("timeout read: %d\n", tout);  //DEBUG
                 tout++;  //DEBUG
                 sleep(1);
             }
@@ -207,26 +219,26 @@ int main(int argc, char* argv[]){
         time(&cur_secs);
 
         if (cur_secs - initial_time >= nsecs){
-            fprintf(stderr, "[server] Execution time has ended!\n");
+            fprintf(stderr,"[server] timeout reached: %ld\n", cur_secs);
             time_is_up = true;
         }
     }
 
     /*-------------------------ENDING PROGRAM-------------------------*/
 
+    //closing public fifo
+    close(public_fd);
+
+    //deleting public fifo file
+    if(remove(public_fifo) != 0){
+        fprintf(stderr, "Not successfully deleted public file\n");
+    }
+
     //main thread waits for all threads to exit
     pthread_exit(NULL);
 
     //releasing pthread mutex structure
     pthread_mutex_destroy(&lock);
-
-    //closing public fifo
-    close(public_fd);
-
-    //deleting public fifo file
-     if (remove(public_fifo) != 0){
-      fprintf(stderr, "Not successfully deleted public file\n");
-     }
 
     return 0;
 }
